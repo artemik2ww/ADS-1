76ab0ffea3cfb9aef1258dc6aeccbce870445394
Артём <71376415+artemik2ww@users.noreply.github.com>
2026-03-02 22:19:58 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,26 +4,59 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if (value < 2){
+    return false;
+  }
+  for (uint64_t i = 2; i*i <= value;i++){
+    if (value % i == 0){
+      return false;
+    }
+  }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  uint64_t k = 0;
+  if (n == 0){
+    return 0;
+  }
+  for (uint64_t i = 2 ; ; i++){
+    if (checkPrime(i)){
+      k++;
+      if (k == i){
+        return i;
+      }
+    }
+  }
+  return k;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  if ( value == 18446744073709551615){ return value; }
+  for (uint64_t i = value + 1; i <= 18446744073709551615; i++){
+    if (checkPrime(i)){
+      return i;
+    }
+  }
+  return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  uint64_t k = 0;
+  for (uint64_t i = 2; i < hbound; i++){
+    if (checkPrime(i)){
+      k+=i;
+    }
+  }
+  return k;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+  uint64_t k = 0;
+  for (uint64_t i = lbound; i < hbound- 2; i++){
+    if (checkPrime(i) && checkPrime(i+2)){
+      k++;
+    }
+  }
+  return k;
 }
